2b0ff3f7ea99504423345829d472333c52d0a3e0
soohyunau <soohyunau@gmail.com>
2021-02-26 17:43:35 +1000

    TodoInsert function added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useCallback} from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -20,11 +20,26 @@ const App = () => {
       text: 'Make todo list management app',
       checked: false,
     },
-  ])
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+    },
+    [todos],
+  );
 
   return (
   <TodoTemplate>
-    <TodoInsert />
+    <TodoInsert onInsert={onInsert} />
     <TodoList todos={todos} />
   </TodoTemplate>
   );
