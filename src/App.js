a450280b5084da778d466b401c7eb4d9f388a471
soohyunau <soohyunau@gmail.com>
2021-02-26 15:34:05 +1000

    Todo list and item added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Basic react app',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'styling with scss',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'Make todo list management app',
+      checked: false,
+    },
+  ])
+
   return (
   <TodoTemplate>
     <TodoInsert />
+    <TodoList todos={todos} />
   </TodoTemplate>
   );
 }
